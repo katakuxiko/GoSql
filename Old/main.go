16577b5b286457f194c186bb1c0e296c13a1047c
katakuxiko <73806376+katakuxiko@users.noreply.github.com>
2021-10-15 22:11:15 +0600

    GoSql 7

--- Old/main.go
+++ Old/main.go
@@ -1,9 +1,12 @@
 package main
-import("fmt"
-		"database/sql"
-		_ "github.com/go-sql-driver/mysql"
-		
 
+import (
+
+	"fmt"
+	"log"
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 type User struct {
 	Name string `json:"name"`
@@ -11,28 +14,29 @@ type User struct {
 
 }
 func main(){
-	db, err := sql.Open("mysql", "root:Katakuxiko2003alesha@tcp(127.0.0.1:3306)/golang")
+	db, err := sql.Open("mysql", "root:Vinter1973@tcp(127.0.0.1:3306)/golang")
 	if err != nil{
 		panic(err)
 	}
 	defer db.Close()
 
-	// insert, err := db.Query("INSERT INTO `users` (`name`,`age`) VALUES('Alexandr', 45)")
-	// if err != nil{
-	// 	panic(err)
-	// }
-	// defer insert.Close()
+	insert, err := db.Query("INSERT INTO `users` (`name`,`age`) VALUES('A', 425)")
+	if err != nil{
+		panic(err)
+	}
+	defer insert.Close()
 	
 	//Выборка
 	res, err := db.Query("SELECT `name`,`age` FROM `users` ")
 	if err != nil{
-		 	panic(err)
+		log.Println(err)
+			
 		}
 	for res.Next(){
 		var user User
 		err = res.Scan(&user.Name,&user.Age)
 		if err != nil{
-			panic(err)
+			log.Println(err)
 		
 		}
 		fmt.Printf("User: %s with age %d \n", user.Name,user.Age)
---
+++ newm/go.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+type Artiles struct {
+	Id uint16 `json:"id"`
+	Title string `json:"title"`
+	Anons string `json:"anons"`
+	Full_text string `json:"full_text"`
+
+}
+var posts = []Artiles{}
+func index(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("newm/templates/index.html", "newm/templates/header.html", "newm/templates/footer.html")
+	if err != nil {
+		log.Print(err)
+	}
+
+	db, err := sql.Open("mysql", "root:Vinter1973@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	res, err := db.Query("SELECT* FROM `articles`")
+	if err != nil{
+		log.Println(err)	
+		}
+	posts = []Artiles{}	
+	for res.Next(){
+		var post Artiles
+		err = res.Scan(&post.Id,&post.Title,&post.Anons,&post.Full_text)
+		if err != nil{
+			log.Println(err)
+		
+		}
+		posts = append(posts, post)
+
+}
+
+	t.ExecuteTemplate(w, "index", posts	)
+}
+func create(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("newm/templates/create.html", "newm/templates/header.html", "newm/templates/footer.html")
+	if err != nil {
+		log.Print(err)
+	}
+	t.ExecuteTemplate(w, "create", nil)
+}
+func save_articles(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+	full_text := r.FormValue("full_text")
+	if title == "" || full_text == "" || anons == "" {
+		fmt.Fprintf(w, "Не все данные заполнены")
+	}else{
+	db, err := sql.Open("mysql", "root:Vinter1973@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	
+
+	insert, err := db.Query(fmt.Sprintf("INSERT INTO `articles` (`title`,`anons`,`full_text`) VALUES('%s', '%s','%s')", title, full_text, anons))
+	if err != nil {
+		panic(err)
+	}
+	defer insert.Close()
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+}
+
+func handleFunc() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.HandleFunc("/", index)
+	http.HandleFunc("/create", create)
+	http.HandleFunc("/save_articles", save_articles)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func main() {
+	handleFunc()
+
+}
